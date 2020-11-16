a6477ff407a57feca605a64d83afa6d4e5ba0fb6
Victoria Mount <victoriamount01@gmail.com>
2020-11-16 12:39:27 -0600

    Set up CRA, create header component

---
+++ anywhere-fitness/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Anywhere Fitness</h1>
+            <nav>
+                <a href='#'>Dashboard</a>
+                <a href='#'>Log In</a>
+                <a href='#'>Sign Up</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
